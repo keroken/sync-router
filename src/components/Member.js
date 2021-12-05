001ef052a1c0ee25b663b8e9088d21e9ddebbdbd
keroken <kenichi.shinagawa@gmail.com>
2021-12-05 23:03:20 +0900

    add redirect to /list

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,19 +1,21 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import { members } from '../memberData';
 
 const Member = () => {
   const { memberId } = useParams();
-  console.log(memberId);
   const selectedMember = members.filter(member => member.id === memberId)[0];
-  console.log(selectedMember);
 
-  return (
-    <section>
-      <h2>{selectedMember.name}</h2>
-      <p>age: {selectedMember.age}</p>
-    </section>
-  );
+  if (selectedMember) {
+    return (
+      <section>
+        <h2>{selectedMember.name}</h2>
+        <p>age: {selectedMember.age}</p>
+      </section>
+    );
+  }
+  
+  return <Redirect to='/list' />
 };
 
 export default Member;
