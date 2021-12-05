4f543217eeb420dd40637ef9f49d3a2bc8cf617a
keroken <kenichi.shinagawa@gmail.com>
2021-12-05 23:14:04 +0900

    add loading state

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
+import { Redirect, useParams, useLocation } from 'react-router-dom';
 import { members } from '../memberData';
 
 const Member = () => {
   const { memberId } = useParams();
   const selectedMember = members.filter(member => member.id === memberId)[0];
 
+  const { search } = useLocation();
+  const queryParams = new URLSearchParams(search);
+  const isLoading = !!queryParams.get('loading');
+
   if (selectedMember) {
     return (
       <section>
-        <h2>{selectedMember.name}</h2>
-        <p>age: {selectedMember.age}</p>
+        {!isLoading ? (
+          <>
+            <h2>{selectedMember.name}</h2>
+            <p>age: {selectedMember.age}</p>
+          </>
+        ) : (
+          <p>Loading....</p>
+        )}
       </section>
     );
   }
