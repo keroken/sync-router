77b1cb15e91c66f61b761e3524d3f43267ca0544
keroken <kenichi.shinagawa@gmail.com>
2021-12-06 00:08:37 +0900

    create history page and nest it under about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import History from './History';
+import { Link, Switch, Route, useRouteMatch } from 'react-router-dom';
+import styled from 'styled-components';
 
 const About = () => {
+  const match = useRouteMatch();
   return (
     <>
       <Helmet>
@@ -9,9 +13,34 @@ const About = () => {
       </Helmet>
       <div>
         <h2>This is About</h2>
+        <StyledMenu>
+          <li className="menu-item">
+            <Link to={`${match.url}/history`}>History</Link>
+          </li>
+        </StyledMenu>
       </div>
+      <Switch>
+        <Route path={`${match.path}/history`}>
+          <History />
+        </Route>
+      </Switch>
     </>
   );
 };
 
 export default About;
+
+const StyledMenu = styled.ul`
+  display: flex;
+  align-items: center;
+  list-style: none;
+  & .menu-item {
+    margin-right: 24px;
+    & a {
+      text-decoration: none;
+      &:hover {
+        opacity: 0.6;
+      }
+    }
+  }
+`;
