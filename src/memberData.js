b7f42bc6b419d8ff91666125c94665a7ba5bd4ac
keroken <kenichi.shinagawa@gmail.com>
2021-12-05 22:21:34 +0900

    separate member data

---
+++ src/memberData.js
@@ -0,0 +1,27 @@
+export const members = [
+  {
+    id: '1',
+    name: 'Steve',
+    age: '24'
+  },
+  {
+    id: '2',
+    name: 'Amy',
+    age: '21'
+  },
+  {
+    id: '3',
+    name: 'Lisa',
+    age: '18'
+  },
+  {
+    id: '4',
+    name: 'Paul',
+    age: '22'
+  },
+  {
+    id: '5',
+    name: 'Dan',
+    age: '26'
+  },
+];
