75d506c67c0fc241a5b2c95fea862c3be20b2d64
keroken <kenichi.shinagawa@gmail.com>
2021-12-10 23:59:47 +0900

    update css

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { members } from '../memberData';
 import { Helmet } from 'react-helmet';
+import classes from './List.module.css';
 
 const List = () => {
 
@@ -12,13 +13,15 @@ const List = () => {
       </Helmet>
       <div>
         <h2>This is List</h2>
+        <ul className={classes.list}>
         {members.map(member => (
-          <li key={member.id}>
+          <li key={member.id} className={classes['list-item']}>
             <Link to={`member/${member.id}`}>
               <span>{member.name}</span>
             </Link>
           </li>
         ))}
+        </ul>
       </div>
     </>
   );
