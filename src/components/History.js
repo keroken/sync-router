aa4f6aec08fba482587c976954689913c741fd95
keroken <kenichi.shinagawa@gmail.com>
2021-12-11 19:53:36 +0900

    add count button to history

--- src/components/History.js
+++ src/components/History.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const History = () => {
+  const [count, setCount] = useState(0);
   return (
     <div>
       <h2>This is History</h2>
+      <div>
+        <button disabled={count === 3} onClick={() => setCount(prev => prev + 1)}>Click Me</button>
+      </div>
+      <span>{count}</span>
     </div>
   );
 };
